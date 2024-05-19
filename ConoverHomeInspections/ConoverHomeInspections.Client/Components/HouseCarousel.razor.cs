83d97d4f9e571f2f904fb847d54f3ec09787c98e
Oliver Conover <fuchsfarbeart@outlook.com>
2024-05-19 04:17:41 -0400

    Updated UI layout and implemented HouseCarousel component
    Implemented changes to the UI to improve responsiveness and aesthetics. Shuffled around components including main header, footer, main layout and navigation menu. A HouseCarousel component was also created to better manage the image carousel functionality. Transferred several files between collaborations for better accessibility.

---
+++ ConoverHomeInspections/ConoverHomeInspections.Client/Components/HouseCarousel.razor.cs
@@ -0,0 +1,87 @@
+using ConoverHomeInspections.Shared;
+using Microsoft.AspNetCore.Components;
+using MudBlazor;
+
+namespace ConoverHomeInspections.Client.Components
+{
+    public partial class HouseCarousel : ComponentBase
+    {
+        private List<ImageData> _houseImages = new List<ImageData>
+                                               {
+                                                   new ImageData()
+                                                   {
+                                                       ImgName = "House 1",
+                                                       ImgUrl = "images/house-1.jpg",
+                                                       ImgAlt = "Picture of a house."
+                                                   },
+                                                   new ImageData()
+                                                   {
+                                                       ImgName = "House 2",
+                                                       ImgUrl = "images/house-2.jpg",
+                                                       ImgAlt = "Picture of a house."
+                                                   },
+                                                   new ImageData()
+                                                   {
+                                                       ImgName = "House 3",
+                                                       ImgUrl = "images/house-3.jpg",
+                                                       ImgAlt = "Picture of a house."
+                                                   },
+                                                   new ImageData()
+                                                   {
+                                                       ImgName = "House 4",
+                                                       ImgUrl = "images/house-4.jpg",
+                                                       ImgAlt = "Picture of a house."
+                                                   },
+                                                   new ImageData()
+                                                   {
+                                                       ImgName = "House 5",
+                                                       ImgUrl = "images/house-5.jpg",
+                                                       ImgAlt = "Picture of a house."
+                                                   },
+                                                   new ImageData()
+                                                   {
+                                                       ImgName = "House 6",
+                                                       ImgUrl = "images/house-6.jpg",
+                                                       ImgAlt = "Picture of a house."
+                                                   },
+                                                   new ImageData()
+                                                   {
+                                                       ImgName = "House 7",
+                                                       ImgUrl = "images/house-7.jpg",
+                                                       ImgAlt = "Picture of a house."
+                                                   },
+                                                   new ImageData()
+                                                   {
+                                                       ImgName = "House 8",
+                                                       ImgUrl = "images/house-8.jpg",
+                                                       ImgAlt = "Picture of a house."
+                                                   },
+                                                   new ImageData()
+                                                   {
+                                                       ImgName = "House 9",
+                                                       ImgUrl = "images/house-9.jpg",
+                                                       ImgAlt = "Picture of a house."
+                                                   },
+                                                   new ImageData()
+                                                   {
+                                                       ImgName = "House 10",
+                                                       ImgUrl = "images/house-10.jpg",
+                                                       ImgAlt = "Picture of a house."
+                                                   },
+                                                   new ImageData()
+                                                   {
+                                                       ImgName = "House 11",
+                                                       ImgUrl = "images/house-11.jpg",
+                                                       ImgAlt = "Picture of a house."
+                                                   },
+                                                   new ImageData()
+                                                   {
+                                                       ImgName = "House 12",
+                                                       ImgUrl = "images/quarryville_house.jpg",
+                                                       ImgAlt = "A rustic house in Quarryville, PA. It's covered by the shade of a tree, the sky is gray, and the grass is green. There is a brown, wooden, patio leading up to the front door set in the center of the house's face."
+                                                   },
+                                               };
+
+
+    }
+}
