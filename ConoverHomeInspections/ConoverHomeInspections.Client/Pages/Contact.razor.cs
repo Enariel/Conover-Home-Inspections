917773fe0680bfece2742ab3c75dffdd2ddce3ba
Oliver Conover <fuchsfarbeart@outlook.com>
2024-05-29 14:23:46 -0400

    Refactor contact form validation and layout
    Updated the validation process in the contact form component to use a mud form. Additionally, optimized layout updates in the form elements of the contact page for enhanced clarity and usability. Removed redundant codes.

--- ConoverHomeInspections/ConoverHomeInspections.Client/Pages/Contact.razor.cs
+++ ConoverHomeInspections/ConoverHomeInspections.Client/Pages/Contact.razor.cs
@@ -9,11 +9,14 @@ namespace ConoverHomeInspections.Client.Pages
     public partial class Contact : ComponentBase
     {
         private ClientContactDTO _model = new ClientContactDTO();
-        private DateRange _dateRange = new DateRange();
         private DateTime _minDate = DateTime.Now.Date;
         private DateTime _maxDate = DateTime.Now.Date.AddMonths(1);
+        private DateRange _dateRange = new DateRange
+                                       {
+                                           Start = DateTime.Now,
+                                           End = DateTime.Now.AddDays(1)
+                                       };
         private string DateHelperText => $"{_dateRange.Start:M} to {_dateRange.End:M}";
-
         private Dictionary<string, string> _stateDict = new Dictionary<string, string>()
                                                         {
                                                             { "Alabama", "AL" }, { "Alaska", "AK" }, { "Arizona", "AZ" }, { "Arkansas", "AR" }, { "American Samoa", "AS" }, { "California", "CA" }, { "Colorado", "CO" }, { "Connecticut", "CT" }, { "Delaware", "DE" }, { "District of Columbia", "DC" }, { "Florida", "FL" }, { "Georgia", "GA" }, { "Guam", "GU" }, { "Hawaii", "HI" }, { "Idaho", "ID" }, { "Illinois", "IL" }, { "Indiana", "IN" }, { "Iowa", "IA" }, { "Kansas", "KS" }, { "Kentucky", "KY" }, { "Louisiana", "LA" }, { "Maine", "ME" }, { "Maryland", "MD" }, { "Massachusetts", "MA" }, { "Michigan", "MI" }, { "Minnesota", "MN" }, { "Mississippi", "MS" }, { "Missouri", "MO" }, { "Montana", "MT" }, { "Nebraska", "NE" }, { "Nevada", "NV" }, { "New Hampshire", "NH" }, { "New Jersey", "NJ" }, { "New Mexico", "NM" }, { "New York", "NY" }, { "North Carolina", "NC" }, { "North Dakota", "ND" }, { "Northern Mariana Islands", "MP" }, { "Ohio", "OH" }, { "Oklahoma", "OK" }, { "Oregon", "OR" }, { "Pennsylvania", "PA" }, { "Puerto Rico", "PR" }, { "Rhode Island", "RI" }, { "South Carolina", "SC" }, { "South Dakota", "SD" }, { "Tennessee", "TN" }, { "Texas", "TX" }, { "Trust Territories", "TT" }, { "Utah", "UT" }, { "Vermont", "VT" }, { "Virginia", "VA" }, { "Virgin Islands", "VI" }, { "Washington", "WA" }, { "West Virginia", "WV" }, { "Wisconsin", "WI" }, { "Wyoming", "WY" }
@@ -21,6 +24,8 @@ namespace ConoverHomeInspections.Client.Pages
 
         private bool _isSuccess = false;
         private bool _isSubmitting = false;
+        private string[] _errors;
+        private MudForm _form;
 
         [Inject] private ILogger<Contact> Logger { get; set; }
         [Inject] private IContactService ContactService { get; set; }
@@ -35,14 +40,6 @@ namespace ConoverHomeInspections.Client.Pages
             await base.OnParametersSetAsync();
         }
 
-        /// <inheritdoc />
-        protected override async Task OnInitializedAsync()
-        {
-            await base.OnInitializedAsync();
-            _model.InspectionDateRange.Start = DateTime.Now;
-            _model.InspectionDateRange.End = DateTime.Now.AddDays(1);
-        }
-
         // Update the model with the new date information
         private async Task OnRangeChanged(DateRange dateRange)
         {
@@ -52,23 +49,18 @@ namespace ConoverHomeInspections.Client.Pages
             await Task.CompletedTask;
         }
 
-        public async Task OnFormSubmitAsync(EditContext ctx)
+        public async Task OnFormSubmitAsync()
         {
             _isSubmitting = true;
-            _isSuccess = false;
 
+            await _form.Validate();
+            _isSuccess = _form.IsValid;
             // Pass data annotation validations
-            if (!ctx.Validate())
+            if (!_isSuccess)
             {
                 _isSubmitting = false;
-                _isSuccess = false;
-                StateHasChanged();
                 return;
             }
-
-            // Then pass custom validations
-            // Address validations
-            // Realtor validations
 
             Logger.LogInformation("Form being sent to server...");
             await ContactService.ProcessContactFormAsync(_model);
